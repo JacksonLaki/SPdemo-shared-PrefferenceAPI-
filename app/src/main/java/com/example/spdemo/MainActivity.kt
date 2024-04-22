fd93d42ae0e418928ab9705d68770dede568c07f
JacksonLaki <jacksonlazarocs@gmail.com>
2024-04-22 04:46:40 -0700

    SPDemo

--- app/src/main/java/com/example/spdemo/MainActivity.kt
+++ app/src/main/java/com/example/spdemo/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.spdemo
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameText:EditText
+    private lateinit var ageText:EditText
+    private lateinit var sf:SharedPreferences
+    private lateinit var editor:SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        nameText = findViewById(R.id.etName)
+        ageText = findViewById(R.id.etAge)
+        sf = getSharedPreferences("my-sf", MODE_PRIVATE)
+        editor = sf.edit()
 
     }
 }
